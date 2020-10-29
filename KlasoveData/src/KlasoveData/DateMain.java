fdbd4616e807a39e72ef71b15bbe9f5da5b408ac
Dobri <Dobri@DESKTOP-33M0NFR>
2020-10-29 18:16:57 +0200

    DateMain

--- KlasoveData/src/KlasoveData/DateMain.java
+++ KlasoveData/src/KlasoveData/DateMain.java
@@ -6,9 +6,44 @@ public class DateMain {
 		// TODO Auto-generated method stub
 
 		Date d = new Date(10,34,4,6,5,2014);
-		Date e = new Date(19,14,4,20,9,2018);
+		//Date e = new Date(19,14,4,20,9,2018);
 		
 		
+		
+		
+		
+		
+		Event event = new Event("Ime", d);
+		
+		System.out.println("The Name is: " + event.GetName());
+		System.out.println("The Date is: " + event.GetDate());
+		
+		event.ToString();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
 		d.GodinaCheck();
 		d.SekIncrease();
 		d.SedmicaCount();
@@ -27,5 +62,10 @@ public class DateMain {
 		
 		
 	}
-
+*/
+	
+		
+		
+		
+	}
 }
